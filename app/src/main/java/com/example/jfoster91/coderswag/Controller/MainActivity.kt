261b2c391a2013970c7bd0e56cfa2148a2d2afcd
Jonathan  Foster <jfoster91@192.168.1.108>
2018-07-22 15:21:23 +0100

    lesson 02

--- app/src/main/java/com/example/jfoster91/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/jfoster91/coderswag/Controller/MainActivity.kt
@@ -2,12 +2,22 @@ package com.example.jfoster91.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.example.jfoster91.coderswag.Model.Category
 import com.example.jfoster91.coderswag.R
+import com.example.jfoster91.coderswag.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter: ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, DataService.categories)
+
+        categoriesListView.adapter = adapter
     }
 }
--- app/src/main/java/com/example/jfoster91/coderswag/Services/DataService.kt
+++ app/src/main/java/com/example/jfoster91/coderswag/Services/DataService.kt
@@ -17,7 +17,7 @@ object DataService {
             Product("Devslopes Graphic Beanie", "$18", "hat01"),
             Product("Devslopes Hat Black", "$20", "hat02"),
             Product("Devslopes Hat White", "$22", "hat03"),
-            Product("Devslopes Snapback", "$18", "hat04"),
+            Product("Devslopes Snapback", "$18", "hat04")
 
     )
 
@@ -25,7 +25,7 @@ object DataService {
             Product("Devlopes Hoodies Grey", "$28", "hoodie01"),
             Product("Devlopes Hoodies Red", "$32", "hoodie02"),
             Product("Devlopes Hoodies Black", "$28", "hoodie03"),
-            Product("Devlopes Hoodies Silver", "$45", "hoodie04"),
+            Product("Devlopes Hoodies Silver", "$45", "hoodie04")
 
     )
 
@@ -34,6 +34,6 @@ object DataService {
             Product("Devlopes Badge Light Grey", "$22", "hoodie02"),
             Product("Devlopes Logo Red", "$22", "hoodie03"),
             Product("Devlopes Hustle", "$20", "hoodie04"),
-            Product("Kickflip Studio", "$18", "hoodie05")
+            Product("Kickflip Studio", "$18", "hoodie05"))
 
 }
